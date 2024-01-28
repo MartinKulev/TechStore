6d6f47e8b73be128894b3fe1d91b4f3c36cdd00d
Martin Kulev <martindkulev@gmail.com>
2024-01-28 21:23:42 +0200

    Fixed button's design and layout

--- obj/Debug/net8.0/TechStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212de52c459b0120406fe29e7cb10f3fa8f785a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
