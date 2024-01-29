05c46c07dc5e0ac79c651ea53ad1dbec16055112
Martin Kulev <martindkulev@gmail.com>
2024-01-29 12:37:00 +0200

    Made the bar visible on all pages

--- Controllers/TechController.cs
+++ Controllers/TechController.cs
@@ -48,43 +48,451 @@ namespace TechStore.Controllers
             {
                 return RedirectToAction("Speakers");
             }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
 
-        public IActionResult Laptops()
+        public IActionResult Laptops(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
-        public IActionResult Smartphones()
+        public IActionResult Smartphones(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
-        public IActionResult Tablets()
+        public IActionResult Tablets(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
-        public IActionResult TVs()
+        public IActionResult TVs(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
-        public IActionResult Monitors()
+        public IActionResult Monitors(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
-        public IActionResult Keyboards()
+        public IActionResult Keyboards(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
-        public IActionResult Mice()
+        public IActionResult Mice(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
-        public IActionResult Headphones()
+        public IActionResult Headphones(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
-        public IActionResult Speakers()
+        public IActionResult Speakers(string optionButton)
         {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
+            return View();
+        }
+        public IActionResult ShoppingCart(string optionButton)
+        {
+            if (optionButton == "laptopsButton")
+            {
+                return RedirectToAction("Laptops");
+            }
+            else if (optionButton == "smartphonesButton")
+            {
+                return RedirectToAction("Smartphones");
+            }
+            else if (optionButton == "tabletsButton")
+            {
+                return RedirectToAction("Tablets");
+            }
+            else if (optionButton == "tvsButton")
+            {
+                return RedirectToAction("TVs");
+            }
+            else if (optionButton == "monitorsButton")
+            {
+                return RedirectToAction("Monitors");
+            }
+            else if (optionButton == "keyboardsButton")
+            {
+                return RedirectToAction("Keyboards");
+            }
+            else if (optionButton == "miceButton")
+            {
+                return RedirectToAction("Mice");
+            }
+            else if (optionButton == "headphonesButton")
+            {
+                return RedirectToAction("Headphones");
+            }
+            else if (optionButton == "speakersButton")
+            {
+                return RedirectToAction("Speakers");
+            }
+            else if (optionButton == "shoppingCartButton")
+            {
+                return RedirectToAction("ShoppingCart");
+            }
             return View();
         }
     }
