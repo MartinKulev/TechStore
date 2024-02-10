26cb21e6f8ea6479626b3c860ba51d0686f2099b
andgt <andgt@LAPTOP-9CCVPDVF>
2024-02-10 18:21:50 +0200

    register and login options added

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿namespace TechStore.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using TechStore.Models;
+
+    public class AccountController : Controller
+    {
+     
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserModel model)
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
