80c4659013a1cf1ca34973fe185a25fb420d09be
Martin Kulev <martindkulev@gmail.com>
2024-02-24 18:06:19 +0200

    Fixed some product box css stylings

--- obj/Debug/net8.0/TechStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd0ee08e254ded1e72e2e01ec6d5f2a20931f71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285651f85b241cb8cc6c41b1cc323d65a9db6297")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
