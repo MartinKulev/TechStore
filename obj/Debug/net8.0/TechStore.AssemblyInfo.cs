8f6de9b1b8905acc7a44647f46aaa7ba38a3a8c9
Martin Kulev <martindkulev@gmail.com>
2024-02-29 22:52:09 +0200

    Payment page layout finished

--- obj/Debug/net8.0/TechStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e551cac8e59572e72582f3923c92da50d2dcb07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a8bbaa7ccc8234302e215ccb0ac0a1ab4cf22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
