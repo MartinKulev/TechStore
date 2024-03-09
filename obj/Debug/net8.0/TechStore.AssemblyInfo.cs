c0a50d96f3f5622a2426e8f33c18a31f3df19adf
Martin Kulev <martindkulev@gmail.com>
2024-03-09 14:21:53 +0200

    Added required tags, when posting a review

--- obj/Debug/net8.0/TechStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/TechStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a68b36d429721a6fea6e929c3270a9ba479d672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6408f655bc7105a98c55d6eae34ad1f948d5be90")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
